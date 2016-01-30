7999215d137be0edbbdeafacd27d30db26547713
Rene Elias <reneelias03@gmail.com>
2016-01-29 21:31:44 -0800

    StaminaBar is completed in the PreFabs folder - Call DecreaseBar to lower the value of the StaminaBar    - Pass in the int amount out of 100 that you should decrement by    - DecreaseBar returns true if the character has died, false if not

---
+++ RitualFighter/Assets/StaminaBarScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StaminaBarScript : MonoBehaviour {
+
+    public Image FillImage;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+    void Update () {
+
+	}
+
+    public bool DecreaseBar(int decreaseAmount)
+    {
+        float percentModifier = (float)decreaseAmount / 100f;
+        float newXValue = FillImage.rectTransform.localScale.x - percentModifier;
+
+        if (newXValue < 0)
+        {
+            newXValue = 0;
+        }
+
+        FillImage.rectTransform.localScale = new Vector3(newXValue, FillImage.rectTransform.localScale.y);
+        
+        //Returns true if your health has been completed depleted
+        if (newXValue == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
