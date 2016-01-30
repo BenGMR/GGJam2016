4ba43a03875baaf1ee4e28516bdf66441b7b4d39
Ben Villalobos <villalobosb93@gmail.com>
2016-01-29 23:05:46 -0800

    Continued work on game screen
    added stamina bars to the screen with a test controller to decrease
    them.

---
+++ RitualFighter/Assets/Scripts/GameScreen/InputManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManagerScript : MonoBehaviour {
+
+    public GameObject p1StaminaBar;
+    public GameObject p2StaminaBar;
+
+    StaminaBarScript p1Bar;
+    StaminaBarScript p2Bar;
+
+	void Start () {
+        p1Bar = p1StaminaBar.GetComponent<StaminaBarScript>();
+        p2Bar = p2StaminaBar.GetComponent<StaminaBarScript>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            p1Bar.DecreaseBar(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            p2Bar.DecreaseBar(1);
+        }
+	}
+}
