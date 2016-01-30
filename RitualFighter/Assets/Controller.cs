4e377a40324aaa4992079038b968e9679e29f81a
Rene Elias <reneelias03@gmail.com>
2016-01-30 04:50:43 -0800

    TeamSelect scene mechanics mostly done

--- RitualFighter/Assets/Controller.cs
+++ RitualFighter/Assets/Controller.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
 
+    public GameObject LeftEmpty, RightEmpty, MiddleEmpty;
+    public Image LeftArrow, RightArrow, ControllerImage;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if(LeftArrow.GetComponent<SelectionArrowBehavior>().selected)
+        {
+            ControllerImage.GetComponent<RectTransform>().position = LeftEmpty.GetComponent<RectTransform>().position;
+        }
+        else if(RightArrow.GetComponent<SelectionArrowBehavior>().selected)
+        {
+            ControllerImage.GetComponent<RectTransform>().position = RightEmpty.GetComponent<RectTransform>().position;
+        }
+        else
+        {
+            ControllerImage.GetComponent<RectTransform>().position = MiddleEmpty.GetComponent<RectTransform>().position;
+            RightArrow.GetComponent<SelectionArrowBehavior>().noSelection = true;
+            RightArrow.GetComponent<Image>().color = Color.red;
+            LeftArrow.GetComponent<SelectionArrowBehavior>().noSelection = true;
+            LeftArrow.GetComponent<Image>().color = Color.red;
+        }
 	}
 }
--- RitualFighter/Assets/SelectionArrowBehavior.cs
+++ RitualFighter/Assets/SelectionArrowBehavior.cs
@@ -1,34 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
-public class SelectionArrowBehavior : MonoBehaviour {
+public class SelectionArrowBehavior : MonoBehaviour
+{
 
-    public bool selected = false;
+    public bool selected = false, noSelection = true;
+    bool previousMove = false;
 
     Image image;
-    float alpha = 1f, alphaIncrement = .025f;
+    float alpha = 1f, alphaIncrement = .02f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         image = GetComponent<Image>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (noSelection)
+        {
+            alpha -= alphaIncrement;
+            image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+
+            if (alpha <= 0 || alpha >= 1f)
+            {
+                alphaIncrement *= -1;
+            }
+        }
+        else if (selected && image.color != Color.blue)
+        {
+            image.color = Color.blue;
+        }
+        else if (!selected)
+        {
+            image.color = new Color(0, 0, 0, 0);
+        }
+
+
+            char indexChar = this.name[name.Length - 1];
+            float axisValue = Input.GetAxis("LeftStickHorizontal" + indexChar.ToString());
+
+            if (Math.Abs(axisValue) > .5f)
+            {
+                if (!previousMove)
+                {
+                    if (axisValue < 0)
+                    {
+                        if (this.name == "LeftArrowImage" + indexChar)
+                        {
+                            if (noSelection)
+                            {
+                                selected = true;
+                                noSelection = false;
+                            }
+                            else
+                            {
+                                noSelection = true;
+                                image.color = Color.red;
+                            }
+                        }
+                        else
+                        {
+                            noSelection = false;
+                            selected = false;
+                        }
+                    }
+                    else
+                    {
+                        if (this.name == "RightArrowImage" + indexChar)
+                        {
+                            if (noSelection)
+                            {
+                                selected = true;
+                                noSelection = false;
+                            }
+                            else
+                            {
+                                noSelection = true;
+                                image.color = Color.red;
+                            }
+                        }
+                        else
+                        {
+                            noSelection = false;
+                            selected = false;
+                        }
+                    }
+
+                    previousMove = true;
+                }
+            }
+            else
+            {
+                previousMove = false;
+            }
         
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	   if(!selected)
-       {
-           alpha -= alphaIncrement;
-           image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
-           if(alpha <= 0 || alpha >= 1f)
-           {
-               alphaIncrement *= -1;
-           }
-       }
-       else if(image.color != Color.blue)
-       {
-           image.color = Color.blue;
-       }
-	}
+    }
 }
