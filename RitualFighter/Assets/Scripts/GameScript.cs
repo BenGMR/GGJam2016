1e7e00550e8bc389c8e3f60146f62212ed0d50c0
Ben Villalobos <villalobosb93@gmail.com>
2016-01-30 09:30:13 -0800

    Winning looks cooler now
    Particle engine starts when someone wins, text flashes

--- RitualFighter/Assets/Scripts/GameScript.cs
+++ RitualFighter/Assets/Scripts/GameScript.cs
@@ -1,50 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameScript : MonoBehaviour {
 
+    public enum CurrentGameState
+    {
+        Playing,
+        //the short time between someone losing and the game deciding that they lost or won.
+        DelayBeforeWin,
+        //yay!
+        SomeoneWins
+    }
+
+    public CurrentGameState currentGameState = CurrentGameState.Playing;
+    public ParticleSystem volcanoParticles;
+    public Text winText;
+
     bool loseTimerStart = false;
     float elapsedTime = 0;
-    float timeUntilPersonLoses = 3;
+    float timeUntilPersonLoses = 1;
+    float timePerFlash = 0.5f;
 
     string playerThatLost = "None";
 
 	void Start () {
-	
+        winText.gameObject.SetActive(false);
 	}
 	
 	void Update () {
+        if(currentGameState == CurrentGameState.Playing)
+        {
 
-        if (loseTimerStart)
+        }
+        else if (currentGameState == CurrentGameState.DelayBeforeWin)
         {
             elapsedTime += Time.deltaTime;
             if(elapsedTime >= timeUntilPersonLoses)
             {
-                if(playerThatLost == "Player1")
+                volcanoParticles.Play();
+                currentGameState = CurrentGameState.SomeoneWins;
+                if (playerThatLost == "Player1")
                 {
-                    Debug.Log("Player 2 Wins!");
+                    winText.text = "Player 2 Wins!";
                 }
                 else if(playerThatLost == "Player2")
                 {
-                    Debug.Log("Player 1 Wins!");
+                    winText.text = "Player 1 Wins!";
                 }
+                elapsedTime = 0;
+            }
+        }
+        else if(currentGameState == CurrentGameState.SomeoneWins)
+        {
+            elapsedTime += Time.deltaTime;
+            if(elapsedTime >= timePerFlash)
+            {
+                elapsedTime = 0;
+                winText.gameObject.SetActive(!winText.gameObject.activeSelf);
             }
         }
 	}
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log(collider.transform.parent.tag);
         if (collider.transform.parent.tag == "Player1")
         {
             playerThatLost = "Player1";
-            loseTimerStart = true;
-            Debug.Log("Ayyyy");
+            currentGameState = CurrentGameState.DelayBeforeWin;
         }
         else if (collider.transform.parent.tag == "Player2")
         {
             playerThatLost = "Player2";
-            loseTimerStart = true;
+            currentGameState = CurrentGameState.DelayBeforeWin;
         }
     }
 }
