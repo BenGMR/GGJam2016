9de86f38607500c4a140e7878ca6d5408223fc2e
Merlup <merlinjackson88@gmail.com>
2016-01-29 22:37:27 -0800

    Added Loading & Unloading Levels
    Added loading and unloading levels. Started mapping controller
    navigation for menus.

---
+++ RitualFighter/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	public void LoadLevel(string level)
+	{
+		SceneManager.LoadScene (level, LoadSceneMode.Single);
+	}
+
+	public void LoadLevelAdditive(string level)
+	{
+		SceneManager.LoadScene(level, LoadSceneMode.Additive);
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName(level));
+		//EditorSceneManager.OpenScene (level, OpenSceneMode.AdditiveWithoutLoading);
+	}
+
+	public void UnloadLevel(string level)
+	{
+		SceneManager.UnloadScene (level);
+	}
+}
