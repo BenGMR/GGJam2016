ff0bef2fd42aea5cb52afe4f59cc7806e2103e70
Rene Elias <reneelias03@gmail.com>
2016-01-30 20:39:49 -0800

    Superficial Readies are done on team selection page

--- RitualFighter/Assets/Scripts/Controller.cs
+++ RitualFighter/Assets/Scripts/Controller.cs
@@ -6,29 +6,57 @@ public class Controller : MonoBehaviour {
 
     public GameObject LeftEmpty, RightEmpty, MiddleEmpty;
     public Image LeftArrow, RightArrow, ControllerImage;
+    public bool ReadyPosition = false, Ready = false;
+    public Image AButtonImage, ReadyImage;
 
 	// Use this for initialization
 	void Start () {
-
+        AButtonImage.enabled = false;
+        ReadyImage.enabled = false; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(LeftArrow.GetComponent<SelectionArrowBehavior>().selected)
+        if(LeftArrow.GetComponent<SelectionArrowBehavior>().selected && !Ready && !ReadyPosition)
         {
             ControllerImage.GetComponent<RectTransform>().position = LeftEmpty.GetComponent<RectTransform>().position;
+            ReadyPosition = true;
+            AButtonImage.enabled = true;
+            AButtonImage.rectTransform.localPosition = new Vector3(ControllerImage.rectTransform.localPosition.x - AButtonImage.rectTransform.rect.width, ControllerImage.rectTransform.localPosition.y);
+            ReadyImage.rectTransform.localPosition = new Vector3(ControllerImage.rectTransform.localPosition.x - ReadyImage.rectTransform.rect.width, ControllerImage.rectTransform.localPosition.y);
         }
-        else if(RightArrow.GetComponent<SelectionArrowBehavior>().selected)
+        else if(RightArrow.GetComponent<SelectionArrowBehavior>().selected && !Ready && !ReadyPosition)
         {
             ControllerImage.GetComponent<RectTransform>().position = RightEmpty.GetComponent<RectTransform>().position;
+            ReadyPosition = true;
+            AButtonImage.enabled = true;
+            AButtonImage.rectTransform.localPosition = new Vector3(ControllerImage.rectTransform.localPosition.x + AButtonImage.rectTransform.rect.width, ControllerImage.rectTransform.localPosition.y);
+            ReadyImage.rectTransform.localPosition = new Vector3(ControllerImage.rectTransform.localPosition.x + ReadyImage.rectTransform.rect.width, ControllerImage.rectTransform.localPosition.y);
         }
-        else
+        else if (Input.GetButton("A" + this.name[name.Length - 1]) && ReadyPosition)
+        {
+            Ready = true;
+            ReadyImage.enabled = true;
+            AButtonImage.enabled = false;
+        }
+        else if (Input.GetButton("B" + this.name[name.Length - 1]) && Ready)
+        {
+            Ready = false;
+            ReadyImage.enabled = false;
+            AButtonImage.enabled = true;
+        }
+        else if (!RightArrow.GetComponent<SelectionArrowBehavior>().selected && !LeftArrow.GetComponent<SelectionArrowBehavior>().selected)
         {
             ControllerImage.GetComponent<RectTransform>().position = MiddleEmpty.GetComponent<RectTransform>().position;
             RightArrow.GetComponent<SelectionArrowBehavior>().noSelection = true;
             RightArrow.GetComponent<Image>().color = Color.red;
             LeftArrow.GetComponent<SelectionArrowBehavior>().noSelection = true;
             LeftArrow.GetComponent<Image>().color = Color.red;
+            ReadyPosition = false;
+            AButtonImage.enabled = false; 
+            Ready = false;
+            ReadyImage.enabled = false; 
         }
+        
 	}
 }
