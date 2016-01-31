53ba553eb70211fb431cdcbf51a7b8f95e341127
Merlup <merlinjackson88@gmail.com>
2016-01-30 16:28:04 -0800

    Added Asssets

---
+++ RitualFighter/Assets/Scripts/ScollUV.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScollUV : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+		Material mat = mr.material;
+		Vector2 offset = mat.mainTextureOffset;
+		offset.x += Time.deltaTime;
+		mat.mainTextureOffset = offset;
+
+
+	
+	}
+}
