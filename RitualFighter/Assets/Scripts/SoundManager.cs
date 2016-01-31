9d3a9b80460279cb80f5a05cfcce1863e1301f4b
Ben Villalobos <villalobosb93@gmail.com>
2016-01-31 09:11:03 -0800

    Working on audio manager

---
+++ RitualFighter/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+    public static SoundManager instance = null;
+	void Start () {
+
+        instance = this;
+
+	}
+	
+	void Update () {
+	
+	}
+
+    public void PlaySingle(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+        sfxSource.Play();
+    }
+}
