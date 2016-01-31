a39fc9003f49359c90ebc95616fa278338f5e46b
Kevin Keyser <Kevin.Keyser@live.com>
2016-01-31 04:17:11 -0800

    f ddf fa

---
+++ RitualFighter/Assets/OptionsManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsManager : MonoBehaviour {
+
+    public static OptionsManager instance;
+    public float soundVolume = 1;
+    public float musicVolume = 1;
+    public bool help = true;
+
+
+    private void Awake()
+    {
+        // if the singleton hasn't been initialized yet
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;//Avoid doing anything else
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
